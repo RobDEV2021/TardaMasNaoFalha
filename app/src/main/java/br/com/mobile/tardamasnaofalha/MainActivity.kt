9e4ed0d219d8d478cbade88b0017af9ae9ef0639
ROBINSON_DEV <robinsoneduardobarbosa@gmail.com>
2022-10-20 22:27:43 -0300

    referenciado tela login na main activity ,efeito botao click,imagem

--- app/src/main/java/br/com/mobile/tardamasnaofalha/MainActivity.kt
+++ app/src/main/java/br/com/mobile/tardamasnaofalha/MainActivity.kt
@@ -2,10 +2,26 @@ package br.com.mobile.tardamasnaofalha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import br.com.mobile.tardamasnaofalha.databinding.LoginBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        LoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.imagemLogin.setImageResource(R.drawable.img_login)
+        binding.fazerLogin.text = "nova mensagem de login"
+
+        binding.botaoLogin.setOnClickListener {
+
+            Toast.makeText(this, "Clicou em login", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
